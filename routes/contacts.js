308405133abfa284628d4a077805f828ca092ba3
Kevin Cross <141079791+octatesystems@users.noreply.github.com>
2024-01-22 10:32:11 +0100

    update repo

--- routes/contacts.js
+++ routes/contacts.js
@@ -7,7 +7,7 @@ const validation = require('../middleware/validate');
 //Read (Get) records from database
 router.get('/', contactsController.getAllData);
 router.get('/:id', contactsController.getSingleData);
-router.post('/', contactsController.createContact);
+router.post('/', validation.saveContact, contactsController.createContact);
 router.put('/:id', validation.saveContact, contactsController.updateContact);
 router.delete('/:id', contactsController.deleteContact);
 
